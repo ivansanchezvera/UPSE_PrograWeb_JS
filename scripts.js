d955a1a921523c0b70fb1996c3fa4e1a6e6a87cb
Ivan <ivansanchez_1988@hotmail.com>
2016-03-19 10:10:25 -0500

    Usando variables de manera basica

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
-alert("Hola JavaScript"
-document.write("<h1>Hola JS UPSE</p>");
-alert("Gracias por visitar");
+console.log("Inicio del programa");
+//alert("Hola JavaScript");
+//document.write("<h1>Hola JS UPSE</p>");
+//alert("Gracias por visitar");
+
+var mensaje = "A lo real G for life";
+alert(mensaje);
+mensaje = "Bienvenidos a la pagina de Mafia Chumi";
+alert(mensaje);
+
+console.log("Programa completado");

dca17bd1622ae8c594a3ba9ebe0592e32f6eeeae
Ivan <ivansanchez_1988@hotmail.com>
2016-03-19 16:01:59 -0500

    Calculadora del tiempo: Dias vividos

--- scripts.js
+++ scripts.js
@@ -1,14 +1,19 @@
 console.log("Inicio del programa");
 
+var edad = prompt("Esta es la calculadora del tiempo. Ingrese su edad: ");
 var segundosPorMinuto = 60;
 var minutosPorHora = 60;
 var horasPorDia = 24;
 var diasPorSemana = 7;
+var diasPorAnio = 365;
 var semanasPorAnio = 52;
 
 var segundosPorDia = segundosPorMinuto * minutosPorHora * horasPorDia;
-document.write("<h1>Calculadora del Tiempo</h1><p>Hay " + segundosPorDia
-	+ " en un dia</p>");
+var segundosVividos =  segundosPorDia * diasPorAnio * edad;
+document.write("<h1>Calculadora del Tiempo</h1><p>Ud ha vivido ya mas de " 
+	+ segundosVividos + " segundos. Aproveche su vida, no le queda ya mucho."
+	+ "</p>");
+
 
 
 console.log("Programa completado");

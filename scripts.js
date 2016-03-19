872a14519a080524437e44ba55e09b7526124aa2
Ivan <ivansanchez_1988@hotmail.com>
2016-03-19 16:55:02 -0500

    Mostrando error de tratar Strings como numeros

--- scripts.js
+++ scripts.js
@@ -1,19 +1,8 @@
 console.log("Inicio del programa");
 
-var edad = prompt("Esta es la calculadora del tiempo. Ingrese su edad: ");
-var segundosPorMinuto = 60;
-var minutosPorHora = 60;
-var horasPorDia = 24;
-var diasPorSemana = 7;
-var diasPorAnio = 365;
-var semanasPorAnio = 52;
-
-var segundosPorDia = segundosPorMinuto * minutosPorHora * horasPorDia;
-var segundosVividos =  segundosPorDia * diasPorAnio * edad;
-document.write("<h1>Calculadora del Tiempo</h1><p>Ud ha vivido ya mas de " 
-	+ segundosVividos + " segundos. Aproveche su vida, no le queda ya mucho."
-	+ "</p>");
-
-
+var peliculasVistas = prompt("Cuantas peliculas haz visto este mes?");
+var seriesVistas = prompt("Cuantas series has visto este mes?");
+var tiempoPantalla = peliculasVistas + seriesVistas;
+alert('Wow! Has visto ' + tiempoPantalla + " veces minimo la pantalla.");
 
 console.log("Programa completado");

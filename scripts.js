ca4c82d812572946412f0bce0560fb29d8893cf0
Ivan <ivansanchez_1988@hotmail.com>
2016-03-19 17:27:34 -0500

    CuentaCuentos: Agregada cuenta regresiva

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
 console.log("Inicio del programa");
+var preguntas = 3;
+var preguntasFaltantes = "Quedan " + preguntas + " preguntas.";
+alert("Este es el juego del cuenta cuentos. Sigue las instrucciones.");
+var sustantivo1 = prompt("Ingrese un Sustantivo. " + preguntasFaltantes);
+preguntas -= 1;
+preguntasFaltantes = "Quedan " + preguntas + " preguntas.";
+var adjectivo1 = prompt("Ingrese un Adjectivo " + preguntasFaltantes);
+preguntas -= 1;
+preguntasFaltantes = "Quedan " + preguntas + " preguntas.";
+var verbo1 = prompt("Ingrese un verbo " + preguntasFaltantes);
 
-var peliculasVistas = prompt("Cuantas peliculas haz visto este mes?");
-var seriesVistas = prompt("Cuantas series has visto este mes?");
-var tiempoPantalla = peliculasVistas + seriesVistas;
-alert('Wow! Has visto ' + tiempoPantalla + " veces minimo la pantalla.");
-
+var cuento = "El " + adjectivo1 + " " + sustantivo1 + " queria " + verbo1 + " en el balcon de su caleta!";
+document.write("<h1>Cuento</h1><p>" + cuento + "</p>");
 console.log("Programa completado");

78065ddca8810802db0d36fa55095b5fdaaddb18
Ivan <ivansanchez_1988@hotmail.com>
2016-03-19 15:57:37 -0500

    Calculadora del tiempo

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
 console.log("Inicio del programa");
-alert("Este es el juego del cuenta cuentos. Sigue las instrucciones.");
-var sustantivo1 = prompt("Ingrese un Sustantivo");
-var adjectivo1 = prompt("Ingrese un Adjectivo");
-var verbo1 = prompt("Ingrese un verbo");
 
-var cuento = "El " + adjectivo1 + " " + sustantivo1 + " queria " + verbo1 + " en el balcon de su caleta!";
-document.write("<h1>Cuento</h1><p>" + cuento + "</p>");
+var segundosPorMinuto = 60;
+var minutosPorHora = 60;
+var horasPorDia = 24;
+var diasPorSemana = 7;
+var semanasPorAnio = 52;
+
+var segundosPorDia = segundosPorMinuto * minutosPorHora * horasPorDia;
+document.write("<h1>Calculadora del Tiempo</h1><p>Hay " + segundosPorDia
+	+ " en un dia</p>");
+
+
 console.log("Programa completado");

30aca7e2d54eea84f0720f9d39fabc8d3c0f9825
Ivan <ivansanchez_1988@hotmail.com>
2016-03-19 23:29:24 -0500

    Juego adivina el numero usando booleanos

--- scripts.js
+++ scripts.js
@@ -1,12 +1,19 @@
 console.log("Inicio del programa");
+var adivinoCorrectamente = false;
 document.write("<h1>Adivina el numero y gana un billo!</h1>");
 var numeroAdivinable = Math.floor(Math.random() * 10) + 1;
 var numeroUsuario = prompt("Que numero crees que es el secreto (del 1 al 10)");
 if(parseInt(numeroUsuario)===numeroAdivinable)
 {
+	adivinoCorrectamente = true;
+	
+}
+
+if(adivinoCorrectamente){
 	document.write("<p>Ganaste un billo</p>");
 }else{
-	document.write("<p>Perdiste la apuesta pelad@</p>");
+	document.write("<p>Perdiste la apuesta pelad@. El numero correcto era: " 
+		+ numeroAdivinable + "</p>");
 }
 
 console.log("Programa completado");

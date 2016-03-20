810d368f9f64e4949aaaa04ec93d5f45212aca05
Ivan <ivansanchez_1988@hotmail.com>
2016-03-19 19:03:18 -0500

    Random: Inicio de uso de numeros aleatorios

--- scripts.js
+++ scripts.js
@@ -1,15 +1,4 @@
 console.log("Inicio del programa");
-var preguntas = 3;
-var preguntasFaltantes = "Quedan " + preguntas + " preguntas.";
-alert("Este es el juego del cuenta cuentos. Sigue las instrucciones.");
-var sustantivo1 = prompt("Ingrese un Sustantivo. " + preguntasFaltantes);
-preguntas -= 1;
-preguntasFaltantes = "Quedan " + preguntas + " preguntas.";
-var adjectivo1 = prompt("Ingrese un Adjectivo " + preguntasFaltantes);
-preguntas -= 1;
-preguntasFaltantes = "Quedan " + preguntas + " preguntas.";
-var verbo1 = prompt("Ingrese un verbo " + preguntasFaltantes);
-
-var cuento = "El " + adjectivo1 + " " + sustantivo1 + " queria " + verbo1 + " en el balcon de su caleta!";
-document.write("<h1>Cuento</h1><p>" + cuento + "</p>");
+var dado = Math.floor(Math.random() * 6) + 1;
+alert("Dado dice:" + dado);
 console.log("Programa completado");

44b410a201b129fe07a1a9dbc7e1ddf60a063c92
Ivan <ivansanchez_1988@hotmail.com>
2016-03-19 22:58:26 -0500

    Juego adivina el numero para practicar condicionales

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
 console.log("Inicio del programa");
-var numUsuario = prompt("Ingrese un numero: ");
-var aleatorio = Math.floor(Math.random() * parseInt(numUsuario)) + 1;
-alert("El numero aleatorio es: " + aleatorio);
+document.write("<h1>Adivina el numero y gana un billo!</h1>");
+var numeroAdivinable = Math.floor(Math.random() * 10) + 1;
+var numeroUsuario = prompt("Que numero crees que es el secreto (del 1 al 10)");
+if(parseInt(numeroUsuario)===numeroAdivinable)
+{
+	document.write("<p>Ganaste un billo</p>");
+}else{
+	document.write("<p>Perdiste la apuesta pelad@</p>");
+}
+
 console.log("Programa completado");

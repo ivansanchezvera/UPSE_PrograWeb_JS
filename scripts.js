b6ce05c57f55ae66fae348d31fe33ca090335d1e
Ivan <ivansanchez_1988@hotmail.com>
2016-03-19 23:58:55 -0500

    Juego Adivina el numero comentado. Practicando comentarios JS

--- scripts.js
+++ scripts.js
@@ -1,6 +1,15 @@
+/*
+Este programa simula un juego de adivinar un numero.
+El numero a adivinar es escogido al comienzo y es un numero al azar.
+El jugador tiene que adivinar el numero. Si no adivina al primer
+intento, se le da una pista y puede intentar una segunda vez.
+Si no adivina en 2 intentos es un LOOOOOOSEEEEEEER!
+Este programa es para practicar condiciones IF-ELSE y comentarios.
+*/
 console.log("Inicio del programa");
 var adivinoCorrectamente = false;
 document.write("<h1>Adivina el numero y gana un billo!</h1>");
+//Escogemos el numero a adivinar entre el 1 y el 10 inclusivo.
 var numeroAdivinable = Math.floor(Math.random() * 10) + 1;
 var numeroUsuario = prompt("Que numero crees que es el secreto (del 1 al 10)");
 if(parseInt(numeroUsuario)===numeroAdivinable)
@@ -10,6 +19,7 @@ if(parseInt(numeroUsuario)===numeroAdivinable)
 }else if(parseInt(numeroUsuario)>numeroAdivinable)
 {
 	alert("Hey! Por alli dicen que el numero adivinable es menor que el que pusiste!");
+	//Segunda oportunidad de adivinar
 	var nuevoIntento = prompt("Intentalo de nuevo");
 	if(parseInt(nuevoIntento)===numeroAdivinable)
 	{
@@ -18,6 +28,7 @@ if(parseInt(numeroUsuario)===numeroAdivinable)
 }else if(parseInt(numeroUsuario)<numeroAdivinable)
 {
 	alert("Me sapearon que el numero es mayor al que dijiste, apuestale con confianza!");
+	//Segunda oportunidad de adivinar
 	var nuevoIntento = prompt("Intentalo de nuevo");
 	if(parseInt(nuevoIntento)===numeroAdivinable)
 	{

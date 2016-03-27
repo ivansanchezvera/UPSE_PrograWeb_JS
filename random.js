9078c510863e843cd243b9fa2eefdbed9f13e684
Ivan <ivansanchez_1988@hotmail.com>
2016-03-26 20:36:47 -0500

    Corregido el manejo de errores en el desafio anterior. Es mejor validar los numeros en la funcion de generar random entre limites que validar al ingreso

--- random.js
+++ random.js
@@ -1,4 +1,10 @@
 function numeroAlAzarEntreLimites( limiteInferior, limiteSuperior ){
+	//Verficiando que sean numeros
+	if(isNaN(limiteInferior)||isNaN(limiteSuperior))
+	{	//Lanzando errores
+		throw new Error("Valor ingresado no corresponde a un numero");;
+	}
+
 	var num = Math.round(Math.random() * (limiteSuperior-limiteInferior)) + limiteInferior;
 	return num;
 }
@@ -6,9 +12,5 @@ function numeroAlAzarEntreLimites( limiteInferior, limiteSuperior ){
 var limiteSuperior = prompt("Ingrese un limite superior para el numero al azar");
 var limiteInferior = prompt("Ingrese un limite inferior para el numero al azar");
 
-if(isNaN(limiteInferior)||isNaN(limiteSuperior))
-{	//Lanzando errores
-	throw new Error("Valor ingresado no corresponde a un numero");;
-}else{
-	alert("Su numero al Azar es: " + numeroAlAzarEntreLimites(parseInt(limiteInferior),parseInt(limiteSuperior)));
-}
+alert("Su numero al Azar es: " + numeroAlAzarEntreLimites(parseInt(limiteInferior),parseInt(limiteSuperior)));
+

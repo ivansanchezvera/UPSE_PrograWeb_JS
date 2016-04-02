f7cfcc65ce0fade06ee33e7c7102a1c95c41df71
Ivan <ivansanchez_1988@hotmail.com>
2016-04-02 11:27:48 -0500

    Refactorizacion: Olvide guardar el archivo con comentarios

--- Refactorizando/js/script.js
+++ Refactorizando/js/script.js
@@ -1,18 +1,24 @@
+/*
+Este programa muestra varios colores RGB al azar por pantalla.
+Fue hecho para demostrar la utilidad de los ciclos for y la refactorizacion.
+Principio DRY.
+*/
 var html = '';
 var rgbColor;
 
+//Devuelve cualquier color RGB al azar
 function obtenerColorRGB()
 {
 	return rgbColor = 'rgb(' + obtenerColor() + ',' + obtenerColor() + ',' + obtenerColor() + ')';
 }
 
+//Obtiene un color individual para formar el color RGB
 function obtenerColor()
 {
 	return Math.floor(Math.random() * 256 );
 }
 
-
-
+//Muestra varios colores RGB por pantalla
 for (var i = 0; i < 10; i++) {
 	rgbColor = obtenerColorRGB();
 	html += '<div style="background-color:' + rgbColor + '"></div>';
